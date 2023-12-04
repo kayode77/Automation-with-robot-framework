d3299a947ac229a0ac9e8fedf91a760f6d6f90dd
kayode77 <79026400+kayode77@users.noreply.github.com>
2023-12-04 21:24:33 +0100

    Delete Framework directory

--- Framework/main.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver=webdriver.Chrome(executable_path="C:\\browserdrivers\\chromedriver.exe")
-driver.get('https://www.jumia.com.ng')
-print(driver.title)
-driver.close()
